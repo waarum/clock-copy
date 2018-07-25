22ac08804ea2445673ebdce0de7cccb1d0a21be0
目良渉 <merawataru@meryouwatarunomacbook-air.local>
2018-07-26 02:21:51 +0900

    Initial Commit

---
+++ 時計　コピー/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  時計　コピー
+//
+//  Created by 目良渉 on 2018/07/26.
+//  Copyright © 2018年 Waarum. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 時計　コピー/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  時計　コピー
+//
+//  Created by 目良渉 on 2018/07/26.
+//  Copyright © 2018年 Waarum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
