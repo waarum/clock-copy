7098e792bb75c71edee32c1d18492b37cf23e0bf
Waarum <merawataru@meryouwatarunomacbook-air.local>
2018-08-18 23:57:33 +0900

    delete declarer func and rewrite constants

--- 時計　コピー/FifthViewController.swift
+++ 時計　コピー/FifthViewController.swift
@@ -8,14 +8,6 @@
 
 import UIKit
 
-func declarer(times:Int) -> [Int] {
-    var timeArray = [Int](repeating: 0, count: times+1)
-    for i in 0...times {
-        timeArray[i] = i
-    }
-    return timeArray
-}
-
 class FifthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     // ピッカーの列数指定
@@ -220,9 +212,9 @@ class FifthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         labelUpDate(nowValue)
     }
     
-    let hours:[Int] = declarer(times:23)
-    let minutes:[Int] = declarer(times: 59)
-    let seconds:[Int] = declarer(times: 59)
+    let hours:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    let minutes:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+    let seconds:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
     let setTime = UserDefaults.standard
     var hourValue:Int = 0
     var minuteValue:Int = 0
