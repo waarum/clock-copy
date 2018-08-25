4ae726071646161beacb05f1ae1977bffbd38d60
Waarum <merawataru@meryouwatarunomacbook-air.local>
2018-08-25 10:17:14 +0900

    make function to cancel timer

--- 時計　コピー/FifthViewController.swift
+++ 時計　コピー/FifthViewController.swift
@@ -128,15 +128,24 @@ class FifthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         if upDater() == -1 {
             // タイマー停止
             timer.invalidate()
-            // Hide remainTimeLabel
-            remainTimeLabel.isHidden = true
-            // Show picker
-            timePicker.isHidden = false
-            // reset count
-            count = 0
+            cancel()
         } 
     }
-    
+    // キャンセル処理
+    func cancel () {
+        // タイマー停止
+        timer?.invalidate()
+        // ボタンを出す
+        startButton.isHidden = false
+        // pickerを出す
+        timePicker.isHidden = false
+        // ラベルを隠す
+        remainTimeLabel.isHidden = true
+        // タイマーをリセット
+        count = 0
+        // ラベルもリセット
+        labelUpDate(nowValue)
+    }
     
     
     // 画面読み込み時
@@ -222,19 +231,9 @@ class FifthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     @IBOutlet weak var cancelButton: UIButton!
     // キャンセルボタンを押した後の操作
     @IBAction func cancelButton(_ sender: Any) {
-        // タイマー停止
-        timer?.invalidate()
-        // ボタンを出す
-        startButton.isHidden = false
-        // pickerを出す
-        timePicker.isHidden = false
-        // ラベルを隠す
-        remainTimeLabel.isHidden = true
-        // タイマーをリセット
-        count = 0
-        // ラベルもリセット
-        labelUpDate(nowValue)
+        cancel()
     }
+    
     
     let hours:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
     let minutes:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
